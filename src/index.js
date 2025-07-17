d3fb2ded369c9c27edf4fdeec9673c9c53f92e14
dsaiprasad07 <dsaiprasad1434@gmail.com>
2025-07-18 00:02:02 +0530

    all setuo configure done

--- src/index.js
+++ src/index.js
@@ -2,26 +2,31 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import mongoose from "mongoose";
-import app from './app.js';
+import {app} from './app.js';
 
 
 
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
 
-connectDB() 
+connectDB()
 
-.then(() => {
-    app.listen( process.env.PORT || 8000, () => {
-        console.log("server running succuessfully at", `${process.env.PORT}`)
+    .then(() => {
 
-    })
-    
-    
-}).catch((err) => {
-    console.log("server not  running ", err)
-});
+       app.get("/", (req,res) => {
+        res.send("your server running")
+       })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("server running succuessfully at", `http://127.0.0.1:${process.env.PORT}`)
+
+        })
+
+
+    }).catch((err) => {
+        console.log("server not  running ", err)
+    });
